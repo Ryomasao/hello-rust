bc3afa297b425779d9b83eaa71ca34cc71ea848f
Ryomasao <konoemario@gmail.com>
2021-09-27 11:08:20 +0900

    mod minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -10,13 +10,23 @@ pub struct Config {
 }
 
 impl Config {
-	pub fn new(args: &[String]) -> Result<Config, &'static str> {
+	pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+		args.next();
+
 		if args.len() < 3 {
 			return Err("引数が足りないよ");
 		}
 
-		let query = args[1].clone();
-		let filename = args[2].clone();
+		let query = match args.next() {
+			Some(arg) => arg,
+			None => return Err("クエリを指定してね"),
+		};
+
+		let filename = match args.next() {
+			Some(arg) => arg,
+			None => return Err("ファイル名を指定してね"),
+		};
+
 		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 		Ok(Config {
 			query,
@@ -47,15 +57,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	let mut results = Vec::new();
-
-	for line in contents.lines() {
-		if line.contains(query) {
-			results.push(line);
-		}
-	}
-
-	results
+	contents
+		.lines()
+		.filter(|line| line.contains(query))
+		.collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
